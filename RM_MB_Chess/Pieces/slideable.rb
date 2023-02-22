de2900c7fe805062622da2ef2779cb242f003a4f
bird <mbird3@gmail.com>
2023-02-22 10:59:53 -0800

    add directions for slideable

--- RM_MB_Chess/Pieces/slideable.rb
+++ RM_MB_Chess/Pieces/slideable.rb
@@ -1,19 +1,27 @@
 module Slideable
 
     HORIZONTAL_AND_VERTICAL DIRS = [
-
+        [0,-1],
+        [0,1],
+        [-1,0],
+        [1,0]
     ].freeze
 
     DIAGONAL_DIRS = [
-
+        [-1,-1],
+        [-1,1],
+        [1,-1],
+        [1,1],
     ].freeze
 
     def horizontal_and_vertical_dirs
         # getter for HORIZONTAL_AND_VERTICAL DIRS
+        HORIZONTAL_AND_VERTICAL
     end
 
     def diagonal_dirs
         # getter for DIAGONAL_DIRS
+        DIAGONAL_DIRS
     end
 
     def moves
